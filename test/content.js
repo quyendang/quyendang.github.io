cfc7a2475f8b1e70e3b20a2eb1729da74d6d9fbc
Quyên <11520320@gm.uit.edu.vn>
2024-06-06 10:26:08 +0700

    Update content.js

--- test/content.js
+++ test/content.js
@@ -85,7 +85,7 @@ httpRequest.onreadystatechange = function() {
 };
 httpRequest.open(
   "GET",
-  "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
+  "https://666079ab5425580055b4139f.mockapi.io/api/v1/products",
   true
 );
 httpRequest.send();
