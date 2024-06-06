c7467544bade9a8125fd0e1e3dc26403fbdedce5
Quyên <11520320@gm.uit.edu.vn>
2024-06-06 10:28:33 +0700

    Update content.js

--- test/content.js
+++ test/content.js
@@ -29,7 +29,7 @@ function dynamicClothingSection(ob) {
   h4.appendChild(h4Text);
 
   let h2 = document.createElement("h2");
-  let h2Text = document.createTextNode("rs  " + ob.price);
+  let h2Text = document.createTextNode(ob.price + " VND");
   h2.appendChild(h2Text);
 
   boxDiv.appendChild(boxLink);
